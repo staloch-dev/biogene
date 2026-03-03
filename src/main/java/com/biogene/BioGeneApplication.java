e6ae3dfe7b8f87d43f7ed986ea4c9c42e7a506a8
Jaison Staloch Junior | Desktop-DevCourse <staloch.dev@gmail.com>
2026-03-03 19:11:36 -0300

    chore: initial commit

---
+++ src/main/java/com/biogene/BioGeneApplication.java
@@ -0,0 +1,13 @@
+package com.biogene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioGeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioGeneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biogene/BioGeneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biogene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioGeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
