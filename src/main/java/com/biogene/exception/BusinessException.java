c7b272fa9577b1ae6b780a73f5aca27ef374e9f7
Jaison Staloch Junior | Desktop-DevCourse <staloch.dev@gmail.com>
2026-03-09 21:30:14 -0300

    feat: create generic exceptions

---
+++ src/main/java/com/biogene/exception/BusinessException.java
@@ -0,0 +1,13 @@
+package com.biogene.exception;
+
+public class BusinessException extends RuntimeException{
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
